56faf7bb4dd59c7c6ad4a511de845b95239f1b48
Cesar Gimenes <crg@crg.eti.br>
2020-07-23 21:50:59 -0300

    add metadata example

--- database/postgresql/scripts/draft_db.sql
+++ database/postgresql/scripts/draft_db.sql
@@ -6,11 +6,18 @@ CREATE DATABASE gasgo
 
 CREATE TABLE main
 (
-    id         uuid    NOT NULL,
-    type_id    integer NOT NULL DEFAULT 0,
-    value      text    NOT NULL,
-    value_int  integer NOT NULL DEFAULT 0,
-    parent_id  uuid,
+    id             uuid    NOT NULL,
+    metadata_id    integer NOT NULL DEFAULT 0,
+    value          text    NOT NULL,
+    value_int      integer NOT NULL DEFAULT 0,
+    parent_id      uuid,
     PRIMARY KEY (id)
 );
 
+
+CREATE TABLE metadata
+(
+    id             integer NOT NULL,
+    label          text    NOT NULL,
+    PRIMARY KEY (id)
+);
